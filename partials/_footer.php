15c17f5255f3dabfaea4a03986f4522dcb5944d8
Ritheesh Shetty <ritheeshshetty77@gmail.com>
2022-06-06 13:54:12 +0530

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+<center>
+<span>&copy;All Rights Are Reserved.</span> 
+</center>
+</div> 
---
+++ partials/_header.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">V Discuss</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Category
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Sports</a>
+          <a class="dropdown-item" href="#">Programming</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+    
+    <div class="row mx-2">
+
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <?php include "_loginmodal.php"; ?>
+        <?php include "_signupmodal.php"; ?>
+        <!-- <button class="btn btn-success ml-2 bg-dark" data-toggle="modal"  data-target="#loginModal" >Login</button > -->
+        <!-- <button class="btn btn-success mx-2 bg-dark"data-toggle="modal" data-target="#signupModal">Signup</button> -->
+    </div>
+    </form>
+ 
+  </div>
+</nav>
+   
+
